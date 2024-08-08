e93922603cc45fc047bf41247924a1289ab0c68a
Vinicios Neves <vinicios.neves@mac>
2024-08-08 21:35:45 +0100

    video 1

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import { GlobalStyle } from './GlobalStyle/index.jsx'
 import { RecoilRoot } from 'recoil'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GlobalStyle />
     <RecoilRoot>
